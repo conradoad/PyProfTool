eb026bd2c34da500e1ee09e5e2765d3ebbcd829a
Conrado <conradocostaad@yahoo.com.br>
2020-04-02 15:08:50 -0300

    setup.py adicionado

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pyproftool',
+    version='0.1',
+    packages=[''],
+    url='',
+    license='',
+    author='conrado',
+    author_email='conradocostaad@yahoo.com.br',
+    description=''
+)
